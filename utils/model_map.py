e182dc876d33ab861ecc4f1f2243cb88a647208d
huangxiaohaiaichiyu <upc1704050209@gmail.com>
2024-04-08 12:05:47 +0800

    updates

--- utils/model_map.py
+++ utils/model_map.py
@@ -10,7 +10,6 @@ def model_structure(model, img_size):
           + ' ' * 21 + 'weight shape' + ' ' * 21 + '|' \
           + ' ' * 5 + 'number' + ' ' * 5 + '|')
     print('-' * 142)
-    num_para = 0
     type_size = 1  # 如果是浮点数就是4
     macs, params = get_model_complexity_info(model_name, img_size, as_strings=False, print_per_layer_stat=False,
                                              verbose=False)
@@ -21,18 +20,15 @@ def model_structure(model, img_size):
         if len(shape) <= 45:
             shape = shape + (42 - len(shape)) * blank
         each_para = 1
-        for k in w_variable.shape:
-            each_para *= k
-        num_para += each_para
         str_num = str(each_para)
         if len(str_num) <= 10:
             str_num = str_num + (10 - len(str_num)) * blank
 
         print('| {} | {} | {} |'.format(key, shape, str_num))
     print('-' * 142)
-    print('The total number of parameters: ' + str(num_para))
-    print('The parameters of Model {}: {:.2f} M'.format(model._get_name(), num_para * type_size / 1000 / 1000))
+    print('The total number of parameters: ' + str(params))
+    print('The parameters of Model {}: {:.2f} M'.format(model._get_name(), params * type_size / 1000 / 1000))
     print('The Gflops of {}: {:.2f} G'.format(model._get_name(), (2 * int(macs) * 1e-9)))
     print('-' * 142)
 
-    return num_para * 1e-6, macs * 1e-9
+    return params * 1e-6, macs * 1e-9
