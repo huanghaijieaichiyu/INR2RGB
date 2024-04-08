8fc9b2a9d83def02fdb050fec55c6b8d1183263e
huangxiaohaiaichiyu <upc1704050209@gmail.com>
2024-04-08 12:01:31 +0800

    updates

--- models/base_mode.py
+++ models/base_mode.py
@@ -15,9 +15,8 @@ class Generator(nn.Module):
         self.conv4 = nn.Sequential(RepNCSPELAN4(32, 64, 64, 32)
                                    )
         self.conv5 = SPPELAN(64, 64, 32)
-        self.conv6 = nn.Sequential(
-                                   ADown(64, 64),
-                                   nn.Upsample(scale_factor=2))
+        self.conv6 = nn.Sequential(C2f(64, 64))
+
         self.conv7 = nn.Sequential(Conv(80, 64),
                                    ADown(64, 64),
                                    nn.Upsample(scale_factor=2))
@@ -53,10 +52,61 @@ class Generator(nn.Module):
         return x
 
 
+class GeneratorV2(nn.Module):
+
+    def __init__(self) -> None:
+        super(GeneratorV2, self).__init__()
+        self.conv1 = Conv(1, 8, 3)
+        self.conv2 = Conv(8, 16, 5, 2)
+        self.conv3 = RepNCSPELAN4(16, 32, 32, 16)
+        self.conv4 = nn.Sequential(Conv(32, 64, 5, 2),
+                                   RepNCSPELAN4(64, 128, 128, 64)
+                                   )
+        self.conv5 = SPPELAN(128, 128, 64)
+        self.conv6 = nn.Sequential(C2f(128, 64),
+                                   Conv(64, 64, 5, 2))
+        self.conv7 = nn.Sequential(RepNCSPELAN4(64, 64, 64, 32),
+                                   nn.Upsample(scale_factor=2))
+        self.conv8 = nn.Sequential(Conv(192, 64, 3),
+                                   nn.Upsample(scale_factor=2))
+        self.conv9 = nn.Sequential(C2f(96, 48),
+                                   nn.Upsample(scale_factor=2),
+                                   Conv(48, 16, 3))
+        self.conv10 = nn.Sequential(C2f(16, 8),
+                                    Conv(8, 2, 3, act=False)
+                                    )
+        self.tanh = nn.Tanh()
+        self.concat = Concat()
+
+    def forward(self, x):
+        # head net
+        x1 = self.conv1(x)
+        x2 = self.conv2(x1)
+        x3 = self.conv3(x2)
+        x4 = self.conv4(x3)
+        x5 = self.conv5(x4)
+        x6 = self.conv6(x5)
+
+        # neck net
+
+        x7 = self.conv7(x6)
+        x8 = self.concat([x5, x7])
+        x9 = self.conv8(x8)
+        x10 = self.concat([x3, x9])
+        x11 = self.conv9(x10)
+        x12 = self.conv10(x11)
+        x13 = self.tanh(x12)
+
+        x = x13.view(-1, 2, x.shape[2], x.shape[3])
+
+        return x
+
+
 class Discriminator(nn.Module):
     """
     Discriminator model with no activation function
     """
+
     def __init__(self):
         super(Discriminator, self).__init__()
         self.conv_in = nn.Sequential(Conv(2, 16, 3, act=False),
@@ -104,6 +154,8 @@ class Discriminator(nn.Module):
 
 
 if __name__ == '__main__':
-    model = Discriminator()
-    d_params, d_macs = model_structure(model, (2, 256, 256))
+    # model = Discriminator()
+    model_ = GeneratorV2()
+    # d_params, d_macs = model_structure(model, (1, 256, 256))
+    d_params, d_macs = model_structure(model_, (1, 256, 256))
     print(d_params, d_macs)
