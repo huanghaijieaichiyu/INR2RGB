02f08407a62d428de2ac9691045ac45782e02007
huangxiaohaiaichiyu <upc1704050209@gmail.com>
2024-04-21 15:29:51 +0800

    updata

--- models/base_mode.py
+++ models/base_mode.py
@@ -9,28 +9,32 @@ class Generator(nn.Module):
 
     def __init__(self) -> None:
         super(Generator, self).__init__()
-        self.conv1 = Conv(1, 8, 3)
-        self.conv2 = nn.Sequential(Conv(8, 16, 5),
-                                   ADown(16, 16))
-        self.conv3 = C2f(16, 32, 3)
-        self.conv4 = nn.Sequential(Conv(32, 64, 5),
-                                   ADown(64, 64),
-                                   C2f(64, 128, 3)
+        self.conv1 = Conv(1, 8)
+        self.conv2 = nn.Sequential(C2f(8, 16),
+                                   ADown(16, 16),
                                    )
-        self.conv5 = nn.Sequential(SPPELAN(128, 128, 64),
-                                   ADown(128, 128))
-        self.conv6 = nn.Sequential(C2f(128, 64),
-                                   nn.Upsample(scale_factor=2),
-                                   Conv(64, 64, 5))
-        self.conv7 = nn.Sequential(C2f(64, 64),
+        self.conv3 = nn.Sequential(C2f(16, 32),
+                                   Conv(32, 64),
+                                   Conv(64, 128))
+        self.conv4 = nn.Sequential(C2f(128, 256),
+                                   ADown(256, 256)
+                                   )
+        self.conv5 = nn.Sequential(SPPELAN(256, 256, 128),
+                                   ADown(256, 256),
+                                   C2f(256, 128))
+        self.conv6 = nn.Sequential(nn.Upsample(scale_factor=2),
+                                   C2f(128, 64))
+        self.conv7 = nn.Sequential(C2f(64, 32),
                                    nn.Upsample(scale_factor=2))
-        self.conv8 = nn.Sequential(Conv(96, 64, 3),
-                                   nn.Upsample(scale_factor=2),
+        self.conv8 = nn.Sequential(Conv(160, 128),
+                                   nn.Upsample(scale_factor=2)
                                    )
-        self.conv9 = nn.Sequential(C2f(64, 48),
-                                   Conv(48, 16, 3))
-        self.conv10 = nn.Sequential(C2f(16, 8),
-                                    Conv(8, 2, 3, act=False)
+        self.conv9 = nn.Sequential(C2f(128, 64),
+                                   Conv(64, 32)
+                                   )
+        self.conv10 = nn.Sequential(C2f(32, 16),
+                                    Conv(16, 8),
+                                    Conv(8, 2, act=False)
                                     )
         self.tanh = nn.Tanh()
         self.concat = Concat()
@@ -65,41 +69,35 @@ class Discriminator(nn.Module):
 
     def __init__(self):
         super(Discriminator, self).__init__()
-        self.conv_in = nn.Sequential(Conv(2, 16, 3, act=False),
-                                     nn.LeakyReLU())
-        self.conv1 = nn.Sequential(Conv(16, 32, 3, 2, act=False),
-                                   nn.LeakyReLU(),
-                                   Conv(32, 16, 3, 2, act=False),
-                                   nn.LeakyReLU(),
+        self.conv_in = nn.Sequential(Conv(2, 16, 3)
+                                     )
+        self.conv1 = nn.Sequential(Conv(16, 32, 3, 2),
+                                   Conv(32, 16, 3, 2),
                                    nn.MaxPool2d(3, 2, 1),
-                                   Conv(16, 8, 3, 2, act=False),
-                                   nn.LeakyReLU(),
-                                   Conv(8, 4, 3, 2, act=False),
-                                   nn.LeakyReLU(),
+                                   Conv(16, 8, 3, 2),
+                                   Conv(8, 4, 3, 2),
                                    nn.MaxPool2d(3, 2, 1)
                                    )
         self.conv_out = Conv(4, 1, 3, act=False)  # 记得替换激活函数
         self.flat = nn.Flatten()
         self.liner = nn.Sequential(nn.Linear(16, 8),
                                    nn.LeakyReLU(),
-                                   nn.Linear(8, 1))
-        self.sig = nn.Sigmoid()
+                                   nn.Linear(8, 1),
+                                   nn.SiLU())
 
     def forward(self, x):
         x1 = self.conv_in(x)
         x2 = self.conv1(x1)
         x3 = self.conv_out(x2)
         x4 = self.flat(x3)
-        x5 = self.liner(x4)
-
-        x = self.sig(x5)
+        x = self.liner(x4)
 
         return x
 
 
 if __name__ == '__main__':
-    model = Discriminator()
-    # model_ = Generator()
-    d_params, d_macs = model_structure(model, (2, 256, 256))
-    # d_params, d_macs = model_structure(model_, (1, 256, 256))
+    # model = Discriminator()
+    model_ = Generator()
+    # d_params, d_macs = model_structure(model, (2, 256, 256))
+    d_params, d_macs = model_structure(model_, (1, 256, 256))
     print(d_params, d_macs)
--- train.py
+++ train.py
@@ -1,23 +1,4 @@
-from utils.save_path import Path
-from utils.model_map import model_structure
-from utils.loss import BCEBlurWithLogitsLoss
-from models.base_mode import Generator, Discriminator
-from utils.colorful import PSrgb2lab, PSlab2rgb
-from models.base_mode import ConvertV2
-from datasets.data_set import MyDataset
-from tqdm import tqdm
-from torchvision import transforms
-from torch.utils.data import DataLoader
-from torch.utils import tensorboard
-from torch.backends import cudnn
-from torch.cuda.amp import autocast
-from torch import nn
-from timm.optim import Lion, RMSpropTF
-from timm.loss import SoftTargetCrossEntropy
-from skimage.metrics import structural_similarity
-from skimage.metrics import peak_signal_noise_ratio
 import argparse
-import math
 import math
 import os
 import random
@@ -34,13 +15,9 @@ from torch.optim.lr_scheduler import LambdaLR
 from torch.utils import tensorboard
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from torchvision import transforms
 from tqdm import tqdm
 
 from datasets.data_set import MyDataset
-from models.base_mode import Generator, Discriminator
-from utils.color_trans import PSlab2rgb, PSrgb2lab
-from utils.loss import BCEBlurWithLogitsLoss, FocalLoss
 from models.base_mode import Generator, Discriminator
 from utils.color_trans import PSlab2rgb, PSrgb2lab
 from utils.loss import BCEBlurWithLogitsLoss, FocalLoss
@@ -65,11 +42,8 @@ def train(self):
     path = Path(self.save_path)
     os.makedirs(os.path.join(path, 'generator'))
     os.makedirs(os.path.join(path, 'discriminator'))
-    os.makedirs(os.path.join(path, 'generator'))
-    os.makedirs(os.path.join(path, 'discriminator'))
     # 创建训练日志文件
     train_log = path + '/log.txt'
-    train_log_txt_formatter = '{time_str} [Epoch] {epoch:03d} [gLoss] {gloss_str} [dLoss] {dloss_str}\n'
     train_log_txt_formatter = '{time_str} [Epoch] {epoch:03d} [gLoss] {gloss_str} [dLoss] {dloss_str}\n'
 
     args_dict = self.__dict__
@@ -80,15 +54,9 @@ def train(self):
     if self.device == 'cuda':
         device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-    if self.cuDNN:
-        assert device != 'cuda', 'cuDNN only work on cuda!'
-        cudnn.benchmark = True
-
     log = tensorboard.SummaryWriter(log_dir=os.path.join(self.save_path, 'tensorboard'),
                                     filename_suffix=str(self.epochs),
                                     flush_secs=180)
-    set_random_seed(self.seed, deterministic=self.deterministic,
-                    benchmark=self.benchmark)
     set_random_seed(self.seed, deterministic=self.deterministic,
                     benchmark=self.benchmark)
 
@@ -97,16 +65,11 @@ def train(self):
     generator = Generator()
     discriminator = Discriminator()
 
-    generator = Generator()
-    discriminator = Discriminator()
-
     print('-' * 100)
     print('Drawing model graph to tensorboard, you can check it with:http://127.0.0.1:6006 after running tensorboard '
           '--logdir={}'.format(os.path.join(self.save_path, 'tensorboard')))
     log.add_graph(generator, torch.randn(
-        1, 1, self.img_size[0], self.img_size[1]))
-    log.add_graph(generator, torch.randn(
-        1, 1, self.img_size[0], self.img_size[1]))
+        self.batch_size, 1, self.img_size[0], self.img_size[1]))
     print('Drawing dnoe!')
     print('-' * 100)
     print('Generator model info: \n')
@@ -131,17 +94,8 @@ def train(self):
                                                                                                   ' G'.format(d_macs))
         f.writelines('\n' + '-------------------------------------------')
     print('train models at the %s device' % device)
-    f.writelines('\n' + 'The parameters of generator: {:.2f} M'.format(g_params) + '\n' + 'The Gflops of '
-                 'generator: {:.2f}'
-                 ' G'.format(g_macs))
-    f.writelines('\n' + 'The parameters of discriminator: {:.2f} M'.format(d_params) + '\n' + 'The Gflops of '
-                 ' discriminator: {:.2f}'
-                 ' G'.format(d_macs))
-    f.writelines('\n' + '-------------------------------------------')
-    print('train models at the %s device' % device)
     os.makedirs(path, exist_ok=True)
 
-    # 加载数据集
     # 加载数据集
     train_data = MyDataset(self.data, img_size=self.img_size)
 
@@ -151,16 +105,7 @@ def train(self):
                               drop_last=True)
     assert len(train_loader) != 0, 'no data loaded'
 
-    if self.optimizer == 'AdamW' or self.optimizer == 'Adam':
-        g_optimizer = torch.optim.AdamW(
-            params=generator.parameters(), lr=self.lr, betas=(self.b1, self.b2))
-        d_optimizer = torch.optim.AdamW(
-            params=discriminator.parameters(), lr=self.lr, betas=(self.b1, self.b2))
     if self.optimizer == 'AdamW':
-        g_optimizer = torch.optim.AdamW(
-            params=generator.parameters(), lr=self.lr, betas=(self.b1, self.b2))
-        d_optimizer = torch.optim.AdamW(
-            params=discriminator.parameters(), lr=self.lr, betas=(self.b1, self.b2))
         g_optimizer = torch.optim.AdamW(
             params=generator.parameters(), lr=self.lr, betas=(self.b1, self.b2))
         d_optimizer = torch.optim.AdamW(
@@ -170,15 +115,7 @@ def train(self):
             params=generator.parameters(), lr=self.lr, betas=(self.b1, self.b2))
         d_optimizer = torch.optim.Adam(
             params=discriminator.parameters(), lr=self.lr, betas=(self.b1, self.b2))
-        g_optimizer = torch.optim.Adam(
-            params=generator.parameters(), lr=self.lr, betas=(self.b1, self.b2))
-        d_optimizer = torch.optim.Adam(
-            params=discriminator.parameters(), lr=self.lr, betas=(self.b1, self.b2))
     elif self.optimizer == 'SGD':
-        g_optimizer = torch.optim.SGD(
-            params=generator.parameters(), lr=self.lr, momentum=self.momentum)
-        d_optimizer = torch.optim.SGD(
-            params=discriminator.parameters(), lr=self.lr, momentum=self.momentum)
         g_optimizer = torch.optim.SGD(
             params=generator.parameters(), lr=self.lr, momentum=self.momentum)
         d_optimizer = torch.optim.SGD(
@@ -188,15 +125,7 @@ def train(self):
                            lr=self.lr, betas=(self.b1, self.b2))
         d_optimizer = Lion(params=discriminator.parameters(),
                            lr=self.lr, betas=(self.b1, self.b2))
-        g_optimizer = Lion(params=generator.parameters(),
-                           lr=self.lr, betas=(self.b1, self.b2))
-        d_optimizer = Lion(params=discriminator.parameters(),
-                           lr=self.lr, betas=(self.b1, self.b2))
     elif self.optimizer == 'rmp':
-        g_optimizer = RMSpropTF(params=generator.parameters(), lr=self.lr, momentum=self.momentum,
-                                lr_in_momentum=self.lr * self.momentum)
-        d_optimizer = RMSpropTF(params=discriminator.parameters(), lr=self.lr, momentum=self.momentum,
-                                lr_in_momentum=self.lr * self.momentum)
         g_optimizer = RMSpropTF(params=generator.parameters(), lr=self.lr, momentum=self.momentum,
                                 lr_in_momentum=self.lr * self.momentum)
         d_optimizer = RMSpropTF(params=discriminator.parameters(), lr=self.lr, momentum=self.momentum,
@@ -214,7 +143,6 @@ def train(self):
 
     if self.llamb:
         assert not self.coslr, 'do not using tow stagics at the same time!'
-
         def lf(x): return (
             (1 + math.cos(x * math.pi / self.epochs)) / 2) * (1 - 0.2) + 0.2
         LR_G = LambdaLR(
@@ -229,8 +157,6 @@ def train(self):
         loss = nn.MSELoss()
     elif self.loss == 'FocalLoss':
         loss = FocalLoss(nn.BCEWithLogitsLoss())
-    elif self.loss == 'FocalLoss':
-        loss = FocalLoss(nn.BCEWithLogitsLoss())
     elif self.loss == 'bce':
         loss = nn.BCEWithLogitsLoss()
     else:
@@ -239,18 +165,10 @@ def train(self):
     loss = loss.to(device)
     mse = nn.MSELoss()
     mse = mse.to(device)
-    mse = nn.MSELoss()
-    mse = mse.to(device)
 
     img_pil = transforms.ToPILImage()
 
     # 储存loss 判断模型好坏
-    loss_all = [99.]
-    # 寄存器判断模型提前终止条件
-    per_G_loss = 99
-    per_D_loss = 99
-
-    toleration = 0
     loss_all = [99.]
     # 寄存器判断模型提前终止条件
     per_G_loss = 99
@@ -265,16 +183,7 @@ def train(self):
         cudnn.deterministic = True
     generator.train()
     discriminator.train()
-    # 使用cuDNN加速训练
-    if self.cuDNN:
-        cudnn.enabled = True
-        cudnn.benchmark = True
-        cudnn.deterministic = True
-    generator.train()
-    discriminator.train()
     for epoch in range(self.epochs):
-        d_epoch_loss = 0
-        g_epoch_loss = 0
         d_epoch_loss = 0
         g_epoch_loss = 0
         # 断点训练参数设置
@@ -282,28 +191,6 @@ def train(self):
 
             g_path_checkpoint = self.resume[0]
             d_path_checkpoint = self.resume[1]
-        if self.resume != ['']:
-
-            g_path_checkpoint = self.resume[0]
-            d_path_checkpoint = self.resume[1]
-
-            g_checkpoint = torch.load(g_path_checkpoint)  # 加载断点
-            generator.load_state_dict(g_checkpoint['net'])
-            g_optimizer.load_state_dict(g_checkpoint['optimizer'])
-            g_epoch = g_checkpoint['epoch']  # 设置开始的epoch
-            loss.load_state_dict = g_checkpoint['loss']
-
-            d_checkpoint = torch.load(d_path_checkpoint)  # 加载断点
-            discriminator.load_state_dict(d_checkpoint['net'])
-            d_optimizer.load_state_dict(d_checkpoint['optimizer'])
-            d_epoch = d_checkpoint['epoch']  # 设置开始的epoch
-            loss.load_state_dict = d_checkpoint['loss']
-
-            if g_epoch != d_epoch:
-                print('given models are mismatched')
-                raise NotImplementedError
-
-            epoch = g_epoch
 
             g_checkpoint = torch.load(g_path_checkpoint)  # 加载断点
             generator.load_state_dict(g_checkpoint['net'])
@@ -326,7 +213,6 @@ def train(self):
             print('继续第：{}轮训练'.format(epoch + 1))
 
             self.resume = ['']  # 跳出循环
-            self.resume = ['']  # 跳出循环
         print('第{}轮训练'.format(epoch + 1))
         pbar = tqdm(enumerate(train_loader), total=len(train_loader), bar_format='{l_bar}{bar:10}| {n_fmt}/{'
                                                                                  'total_fmt} {elapsed}')
@@ -334,18 +220,12 @@ def train(self):
             target, (img, label) = data
             # print(img)
             # 对输入图像进行处理
-            # print(img)
-            # 对输入图像进行处理
             img_lab = PSrgb2lab(img)
-            gray, a, b = torch.split(img_lab, [1, 1, 1], 1)
+            gray, a, b = torch.split(img_lab, 1, 1)
             color = torch.cat([a, b], dim=1)
             lamb = 128.  # 取绝对值最大值，避免负数超出索引
             gray = gray.to(device)
             color = color.to(device)
-
-            '''img = img.to(device)
-            img_gray = img_2gray(img)
-            img_gray = img_gray.to(device)'''
 
             '''img = img.to(device)
             img_gray = img_2gray(img)
@@ -369,6 +249,7 @@ def train(self):
                 d_optimizer.step()
                 if self.llamb or self.coslr:
                     LR_D.step()
+
                 '''--------------- 训练生成器 ----------------'''
                 fake = generator(gray)
                 g_optimizer.zero_grad()
@@ -387,7 +268,7 @@ def train(self):
                 break
 
             # 图像拼接还原
-            fake_tensor = torch.zeros_like(img.detach(), dtype=torch.float32)
+            fake_tensor = torch.zeros_like(img, dtype=torch.float32)
             fake_tensor[:, 0, :, :] = gray[:, 0, :, :]  # 主要切片位置
             fake_tensor[:, 1:, :, :] = lamb * fake
             fake_img = np.array(
@@ -395,8 +276,6 @@ def train(self):
             # print(fake_img)
             # 加入新的评价指标：PSN,SSIM
             real_pil = img_pil(img[0])
-            psn = peak_signal_noise_ratio(
-                np.array(real_pil, dtype=np.float32) / 255., fake_img / 255., data_range=1)
             psn = peak_signal_noise_ratio(
                 np.array(real_pil, dtype=np.float32) / 255., fake_img / 255., data_range=1)
 
@@ -415,64 +294,44 @@ def train(self):
         d_checkpoint = {
             'net': discriminator.state_dict(),
             'optimizer': d_optimizer.state_dict(),
-            g_checkpoint = {
-                'net': generator.state_dict(),
-                'optimizer': g_optimizer.state_dict(),
-                'epoch': epoch,
-                'loss': loss.state_dict()
-            }
-            d_checkpoint = {
-                'net': discriminator.state_dict(),
-                'optimizer': d_optimizer.state_dict(),
-                'epoch': epoch,
-                'loss': loss.state_dict()
-            }
-            # 保持最佳模型
-            # 保持最佳模型
+            'epoch': epoch,
+            'loss': loss.state_dict()
+        }
+        # 保持最佳模型
 
-            if g_output.item() < min(loss_all):
+        if g_output.item() < min(loss_all):
             torch.save(g_checkpoint, path + '/generator/best.pt')
-            loss_all.append(g_output.item())
-            if g_output.item() < min(loss_all):
-            torch.save(g_checkpoint, path + '/generator/best.pt')
-            loss_all.append(g_output.item())
+        loss_all.append(g_output.item())
 
-            # 保持训练权重
-            torch.save(g_checkpoint, path + '/generator/last.pt')
-            torch.save(d_checkpoint, path + '/discriminator/last.pt')
-            torch.save(g_checkpoint, path + '/generator/last.pt')
-            torch.save(d_checkpoint, path + '/discriminator/last.pt')
+        # 保持训练权重
+        torch.save(g_checkpoint, path + '/generator/last.pt')
+        torch.save(d_checkpoint, path + '/discriminator/last.pt')
 
-            # 写入日志文件
-            to_write = train_log_txt_formatter.format(time_str=time.strftime("%Y_%m_%d_%H:%M:%S"),
-                                                      epoch=epoch + 1,
-                                                      gloss_str=" ".join(
+        # 写入日志文件
+        to_write = train_log_txt_formatter.format(time_str=time.strftime("%Y_%m_%d_%H:%M:%S"),
+                                                  epoch=epoch + 1,
+                                                  gloss_str=" ".join(
                                                       ["{:4f}".format(g_output.item())]),
-                                                      gloss_str=" ".join(
-                                                      ["{:4f}".format(g_output.item())]),
-                                                      dloss_str=" ".join(["{:4f}".format(d_output.item())]))
-            with open(train_log, "a") as f:
+                                                  dloss_str=" ".join(["{:4f}".format(d_output.item())]))
+        with open(train_log, "a") as f:
             f.write(to_write)
 
             # 5 epochs for saving another model
-            if (epoch + 1) % 10 == 0 and (epoch + 1) >= 10:
+        if (epoch + 1) % 10 == 0 and (epoch + 1) >= 10:
             torch.save(g_checkpoint, path + '/generator/%d.pt' % (epoch + 1))
             torch.save(d_checkpoint, path + '/discriminator/%d.pt' %
                        (epoch + 1))
-            torch.save(d_checkpoint, path + '/discriminator/%d.pt' %
-                       (epoch + 1))
-            # 可视化训练结果
-            log.add_images('real', img, epoch + 1)
-            log.add_images('fake', PSlab2rgb(fake_tensor), epoch + 1)
+        # 可视化训练结果
+        log.add_images('real', img, epoch + 1)
+        log.add_images('fake', PSlab2rgb(fake_tensor), epoch + 1)
 
-            log.close()
+    log.close()
 
 
-            def parse_args():
-            parser = argparse.ArgumentParser()  # 命令行选项、参数和子命令解析器
-            parser.add_argument("--data", type=str,
-                                default='../datasets/coco5000', help="path to dataset")
-            default = '../datasets/coco5000', help = "path to dataset")
+def parse_args():
+    parser = argparse.ArgumentParser()  # 命令行选项、参数和子命令解析器
+    parser.add_argument("--data", type=str,
+                        default='../datasets/coco5000', help="path to dataset")
     parser.add_argument("--epochs", type=int, default=1000,
                         help="number of epochs of training")  # 迭代次数
     parser.add_argument("--batch_size", type=int, default=8,
@@ -481,7 +340,7 @@ def train(self):
                         default=(256, 256), help="size of the image")
     parser.add_argument("--optimizer", type=str, default='Adam',
                         choices=['AdamW', 'SGD', 'Adam', 'lion', 'rmp'])
-    parser.add_argument("--num_workers", type=int, default=20,
+    parser.add_argument("--num_workers", type=int, default=10,
                         help="number of data loading workers, if in windows, must be 0"
                         )
     parser.add_argument("--seed", type=int, default=1999, help="random seed")
@@ -494,9 +353,7 @@ def train(self):
     parser.add_argument("--loss", type=str, default='bce',
                         choices=['BCEBlurWithLogitsLoss', 'mse', 'bce',
                                  'FocalLoss'],
-                        'FocalLoss'],
-    help = "loss function")
-        parser.add_argument("--lr", type=float, default=3.5e-4,
+                        help="loss function")
     parser.add_argument("--lr", type=float, default=3.5e-4,
                         help="learning rate, for adam is 1-e3, SGD is 1-e2")  # 学习率
     parser.add_argument("--momentum", type=float, default=0.5,
@@ -507,10 +364,6 @@ def train(self):
                         help="adam: decay of first order momentum of gradient")  # 动量梯度下降第一个参数
     parser.add_argument("--b2", type=float, default=0.999,
                         help="adam: decay of first order momentum of gradient")  # 动量梯度下降第二个参数
-    parser.add_argument("--coslr", type=bool, default=False,
-                        help="using cosine learning decay")
-    parser.add_argument("--llamb", type=bool, default=True,
-                        help="using yolo tactic")
     parser.add_argument("--coslr", type=bool, default=False,
                         help="using cosine learning decay")
     parser.add_argument("--llamb", type=bool, default=False,
