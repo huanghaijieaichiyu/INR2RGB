4d51010bedb0413e48eedea7df5c2f3829702958
huangxiaohaiaichiyu <upc1704050209@gmail.com>
2024-04-21 15:50:33 +0800

    change sigmod to silu

--- models/base_mode.py
+++ models/base_mode.py
@@ -83,7 +83,7 @@ class Discriminator(nn.Module):
         self.conv_out = Conv(4, 1, 3, act=False)  # 记得替换激活函数
         self.flat = nn.Flatten()
         self.liner = nn.Sequential(nn.Linear(16, 8),
-                                   nn.LeakyReLU(),
+                                   nn.SiLU(),
                                    nn.Linear(8, 1),
                                    nn.SiLU())
 
