887e3bc9301aa72144983cf5e2cc94c8144c6931
huangxiaohaiaichiyu <upc1704050209@gmail.com>
2024-04-24 15:52:38 +0800

    updates

--- predict.py
+++ predict.py
@@ -23,7 +23,7 @@ def parse_args():
     parser.add_argument("--data", type=str, default='0',
                         help='path to dataset, and 0 is to open your camara')
     parser.add_argument(
-        "--model", type=str, default='/home/huang/INR2RGB/runs/train(1)/generator/660.pt', help="path to model")
+        "--model", type=str, default='/home/huang/INR2RGB/runs/train/generator/1000.pt', help="path to model")
     parser.add_argument("--batch_size", type=int, default=16,
                         help="size of the batches")  # batch大小
     parser.add_argument("--img_size", type=tuple,
